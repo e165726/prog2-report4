5876e4e2e00d908d46d9e4ca40aac61872ca6303
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2016-11-16 11:44:45 +0900

    Enemy class,step1 version.

---
+++ src/main/jp/ac/uryukyu/ie/e165726/LivingThing.java
@@ -0,0 +1,44 @@
+package jp.ac.uryukyu.ie.e165726;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing(String name,int hitPoint,int attack){
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+    }
+
+    public boolean isDead(){
+        return dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        int damage;
+        if(dead == false) {
+            damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+        }else{
+            damage = 0;
+        }
+        opponent.wounded(damage);
+    }
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+        }
+
+    }
+}
+
